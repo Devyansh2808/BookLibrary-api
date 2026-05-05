dab967a50e1dca7bb6829b442e47ab1fecc1871e
Devyansh <Devyanshraj1511@gmail.com>
2026-05-05 03:15:30 -0400

    add all book routes (CRUD)

--- main.py
+++ main.py
@@ -7,21 +7,13 @@ next_id = 1
 
 
 app = FastAPI()
-#es
 @app.post("/books")
 def create_book(book: BookCreate):
     global next_id
     book_item = BookItem(
         id=next_id,
-        title=book.title,
-        author=book.author,
-        description=book.description,
-        published_date=book.published_date,
-        isbn=book.isbn,
-        pages=book.pages,
-        country=book.country,
+        **book.model_dump(),
         recieved_date=datetime.date.today(),
-        count=1
     )
     books_db[next_id] = book_item
     next_id += 1
@@ -45,16 +37,16 @@ def update_book(book_id: int, book: BookCreate):
         raise HTTPException(status_code=404, detail="Book not found")
     
     updated_book = BookItem(
+        **book.model_dump(),
         id=book_id,
-        title=book.title,
-        author=book.author,
-        description=book.description,
-        published_date=book.published_date,
-        isbn=book.isbn,
-        pages=book.pages,
-        country=book.country,
         recieved_date=existing_book.recieved_date,
         count=existing_book.count
     )
     books_db[book_id] = updated_book
     return updated_book
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    if book_id not in books_db:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return books_db.pop(book_id)
